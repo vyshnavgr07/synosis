8e5071bc320bd877b1b7a2660088ff22dbdddea4
Vyshnav GR <vyshnav.gr@ivorytechnologies.com>
2025-01-25 10:37:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {BrowserRouter} from 'react-router-dom'
+
+
+function App() {
+return (
+<>
+<BrowserRouter>
+
+</BrowserRouter>
+</>
+  )
+}
+import { BrowserRouter } from 'react-router-dom'
+
+export default App
